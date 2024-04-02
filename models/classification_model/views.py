508ecd44d416abf18819b3bf65b2fda0a88132e7
Anchu <chuan14402@gmail.com>
2024-04-02 20:18:36 +0700

    AI classification

--- models/classification_model/views.py
+++ models/classification_model/views.py
@@ -15,19 +15,21 @@ class ClassificationModelAPIView(APIView):
         return Response("ok", status=status.HTTP_200_OK)
 
     def post(self, request):
-        print(request.FILES)
         file = request.FILES['file']
         image = Image.open(file)
         result = model(image)
-
         names = []
+        probs = []
 
         i = 0
         for index in result[0].probs.top5:
-            names.append(result[0].names[index])
+            current_probs = result[0].probs.top5conf[i].item()
+            if current_probs >= 0.4:
+                names.append(result[0].names[index])
+                probs.append(current_probs)
             i = i + 1
 
         return Response({
             'names': names,
-            'probs': result[0].probs.top5conf
+            'probs': probs
         }, status=status.HTTP_200_OK)
